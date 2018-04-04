add5bc36a4f67a7c1a014cb5f5dcb95e708efa53
moonabyss <d-v@ukr.net>
2018-04-04 19:20:35 +0300

    initial commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Vector3 oldVelocity;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void FixedUpdate()
+    {
+        oldVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        rb.velocity = Vector3.Reflect(oldVelocity, contact.normal);
+    }
+}
---
+++ Assets/Carete.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carete : MonoBehaviour {
+    public GameObject ball;
+    public float thrust;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = ball.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (rb.velocity == Vector3.zero)
+            {
+                rb.AddForce(new Vector3(1, 1, 0) * thrust);
+            } 
+        }
+    }
+}
